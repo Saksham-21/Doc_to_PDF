74077a082e069f4238e974a682e9d1134adededc
Saksham Singla <sakshamsingla21@gmail.com>
2024-11-26 01:42:10 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,8 +60,8 @@ export default function DocxToPdfConverter() {
       setProgress(10);
 
       const endpoint = encrypt
-        ? "https://ec2-51-21-128-185.eu-north-1.compute.amazonaws.com/convert-to-encrypted-pdf"
-        : "https://ec2-51-21-128-185.eu-north-1.compute.amazonaws.com/convert-to-pdf";
+        ? "https://docpdfbackend-production.up.railway.app/convert-to-encrypted-pdf"
+        : "https://docpdfbackend-production.up.railway.app/convert-to-pdf";
 
       const response = await fetch(endpoint, {
         method: "POST",
